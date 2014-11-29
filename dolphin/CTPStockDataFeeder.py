d0d982b92685359f900ff17cf8bada25f4fe9e1a
mars <wudi.xjtu@gmail.com>
2014-11-29 13:54:19 +0800

    new commit

---
+++ dolphin/CTPStockDataFeeder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pzyctp.stock import datafeeder
+import time
+
+class CTPL2StockDataFeeder:
+    def __init__(self):
+        self.l2data = datafeeder.DataFeeder('tcp://101.231.210.1:24513', '2011', 'username', 'passwd')
+        self.ExchangeIDDict = {'sh': 'SSE', 'sz': 'SZE'}
+        self.ExchangeIDDict_reverse = {'SSE': 'sh', 'SZE': 'sz'}
+        time.sleep(5)
+
+    def subscribe(self, stockid):
+        #self.l2data.subscrib_L2_market_data(stockid)
+        self.l2data.subscrib_market_data(stockid)
+
+    def get_data(self, stockid):
+        #return self.l2data.get_L2_market_data(stockid)
+        return self.l2data.get_market_data(stockid)
+
+    
+if __name__ == "__main__":
+    import time
+    feeder = CTPL2StockDataFeeder()
+    feeder.subscribe('sz002029')
+    while True:
+        print feeder.get_data('sz002029')
+#        data2 = feeder.get_data('sh601566')
+        time.sleep(2)
+
